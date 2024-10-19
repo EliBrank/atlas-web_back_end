0ef5cba9b8060d29c9aee5723f6cf043bdf111a2
EliBrank <eliasbrinkman@gmail.com>
2024-10-19 09:41:14 -0500

    remove .entries() syntax

--- ES6_basic/10-loops.js
+++ ES6_basic/10-loops.js
@@ -1,6 +1,6 @@
 export default function appendToEachArrayValue(array, appendString) {
   const arrayCopy = [];
-  for (const value of array.entries()) {
+  for (const value of array) {
     arrayCopy.push(appendString + value);
   }
 
