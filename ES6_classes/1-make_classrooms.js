c1692afa2796eeea1fbd1d334267559ad1b6de29
EliBrank <eliasbrinkman@gmail.com>
2024-10-20 11:17:42 -0500

    fix linting issues with syntax

--- ES6_classes/1-make_classrooms.js
+++ ES6_classes/1-make_classrooms.js
@@ -1,8 +1,8 @@
-import ClassRoom from "./0-classroom";
+import ClassRoom from './0-classroom';
 
 export default function initializeRooms() {
   const roomSizes = [19, 20, 34];
-  const newRooms = roomSizes.map(roomSize => new ClassRoom(roomSize));
+  const newRooms = roomSizes.map((roomSize) => new ClassRoom(roomSize));
 
   return newRooms;
 }
