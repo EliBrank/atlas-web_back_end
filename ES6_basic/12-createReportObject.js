54e617e506c09a9aa753b583b24140d31683517b
EliBrank <eliasbrinkman@gmail.com>
2024-10-19 20:13:17 -0500

    fix minor syntax issues

--- ES6_basic/12-createReportObject.js
+++ ES6_basic/12-createReportObject.js
@@ -1,10 +1,10 @@
 export default function createReportObject(employeesList) {
-  const allEmployees = {...employeesList};
+  const allEmployees = { ...employeesList };
 
   return {
     allEmployees,
     getNumberOfDepartments(employees) {
       return Object.keys(employees).length;
-    }
+    },
   };
 }
