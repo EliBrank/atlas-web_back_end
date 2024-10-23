437d46678bba7f0633e301b5718f703fb0e0d39a
EliBrank <eliasbrinkman@gmail.com>
2024-10-23 17:28:03 -0500

    separate out ternary operator to conditionally assign grade value for linter

--- ES6_data_manipulation/4-update_grade_by_city.js
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -3,7 +3,8 @@ export default function updateGradeByCity(studentArray, city, newGrades) {
     .filter((student) => student.location === city)
     .map((student) => {
       // gets grade associated with student
-      let grade = newGrades.find((currentGrade) => currentGrade.studentId === student.id)?.grade;
+      const gradeObj = newGrades.find((currentGrade) => currentGrade.studentId === student.id);
+      let grade = gradeObj ? gradeObj.grade : undefined;
 
       // checks if grade exists for student and is 0-100, assigned N/A otherwise
       if (!grade || !(grade >= 0 && grade <= 100)) {
