005cf933bc6e9acaf4b003a2062391db03bf8144
EliBrank <eliasbrinkman@gmail.com>
2024-10-25 16:45:23 -0500

    remove curly braces from then() callback to implicitly return contents

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -5,11 +5,9 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
   const promises = [signUpUser(firstName, lastName), uploadPhoto(fileName)];
 
   return Promise.allSettled(promises)
-    .then((results) => {
-      results.map((result) => ({
-        status: result.status,
-        // assign value or error to key depending on status
-        value: result.status === 'fulfilled' ? result.value : result.reason,
-      }));
-    });
+    .then((results) => results.map((result) => ({
+      status: result.status,
+      // assign value or error to key depending on status
+      value: result.status === 'fulfilled' ? result.value : result.reason,
+    })));
 }
