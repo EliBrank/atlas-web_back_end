80144921a7d7656d4db215a8433d220c75ad62cd
EliBrank <ezb.business@pm.me>
2025-01-08 17:33:08 +0000

    define concat function

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+"""Defines concat function"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """Concatenates two input strings into one
+
+    Args:
+        str1: first string
+        str2: second string
+
+    Returns:
+        The concatenated string, str1 + str2
+    """
+
+    return str1 + str2
