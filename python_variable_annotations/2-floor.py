428fe6fc440edd11fe569f90152f3f803b58f29a
EliBrank <ezb.business@pm.me>
2025-01-08 17:42:15 +0000

    define conversion functions floor and to_str

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+"""Defines floor function"""
+
+
+def floor(n: float) -> int:
+    """Finds floor of input float value
+
+    Args:
+        n: input float
+
+    Returns:
+        Input, rounded down (converted to integer)
+    """
+
+    return int(n)
---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+"""Defines to_str function"""
+
+
+def to_str(n: float) -> str:
+    """Converts input float value to string
+
+    Args:
+        n: input float
+
+    Returns:
+        String representation of input
+    """
+
+    return str(n)
