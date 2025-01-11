93e2d5b57c7b1ee9757fb98e4ca6c504ba1ccdb6
EliBrank <ezb.business@pm.me>
2025-01-11 03:32:51 +0000

    use time module to measure execution time of wait_n

---
+++ python_async_function/2-measure_runtime.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+"""Defines measure_time async function"""
+
+import asyncio
+from time import time
+
+
+wait_n = __import__('1-concurrent_coroutines').wait_n
+
+def measure_time(n: int, max_delay: int = 10) -> float:
+    """Measures time taken to execute wait_n
+
+    Args:
+        n: number of wait_random calls for wait_n
+        max_delay: maximum delay passed to wait_n
+
+    Returns:
+        Time taken to execute wait_n, averaged per number of wait_random calls
+    """
+
+    start_time = time()
+
+    asyncio.run(wait_n(n, max_delay))
+
+    end_time = time()
+
+    return (end_time - start_time) / n
