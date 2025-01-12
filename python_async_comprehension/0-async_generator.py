57a38a71a3f09325aeb0ba40200736b127969831
EliBrank <ezb.business@pm.me>
2025-01-12 02:02:12 +0000

    change AsyncGenerator to just Generator

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -4,10 +4,10 @@
 
 import asyncio
 from random import uniform
-from typing import AsyncGenerator
+from typing import Generator
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator() -> Generator[float, None, None]:
     """Yields 10 random numbers between 0 and 10 with 1 second delay
 
     Returns:
