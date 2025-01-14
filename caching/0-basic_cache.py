8c6d77b22da6b4be3a9767466a50f1313bc66b79
EliBrank <ezb.business@pm.me>
2025-01-14 17:24:56 +0000

    create basic cache class with new get, put methods

--- caching/0-basic_cache.py
+++ caching/0-basic_cache.py
@@ -2,9 +2,25 @@
 
 """creates the BasicCache class"""
 
-from caching.base_caching import BaseCaching
+from base_caching import BaseCaching
 
 
 class BasicCache(BaseCaching):
     """instantiates a caching system"""
 
+    def __init__(self):
+        """Instantiates BasicCache with cache_data from BaseCaching"""
+        super().__init__()
+
+    def put(self, key, item):
+        """add an item in the cache"""
+        if not (key and item):
+            return
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """get an item via its key"""
+        if not key or key not in self.cache_data:
+            return None
+        else:
+            return self.cache_data[key]
