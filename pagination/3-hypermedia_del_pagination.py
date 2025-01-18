a7c3cc78ff16a8313f3f0d00b110dfaf9eb2ac34
EliBrank <ezb.business@pm.me>
2025-01-18 00:48:40 +0000

    create method which keeps track of indices while building data list

--- pagination/3-hypermedia_del_pagination.py
+++ pagination/3-hypermedia_del_pagination.py
@@ -1,15 +1,12 @@
 #!/usr/bin/env python3
-"""
-Deletion-resilient hypermedia pagination
-"""
+
+"""Deletion-resilient hypermedia pagination"""
 
 import csv
-import math
 
 
 class Server:
-    """Server class to paginate a database of popular baby names.
-    """
+    """Server class to paginate a database of popular baby names."""
     DATA_FILE = "Popular_Baby_Names.csv"
 
     def __init__(self):
@@ -28,8 +25,7 @@ class Server:
         return self.__dataset
 
     def indexed_dataset(self) -> dict[int, list]:
-        """Dataset indexed by sorting position, starting at 0
-        """
+        """Dataset indexed by sorting position, starting at 0"""
         if self.__indexed_dataset is None:
             dataset = self.dataset()
             truncated_dataset = dataset[:1000]
@@ -38,8 +34,9 @@ class Server:
             }
         return self.__indexed_dataset
 
-    def get_hyper_index(self, index: int | None = None, page_size: int = 10) -> dict:
-        """Retrieves various information for page of dataset (deletion resilient)
+    def get_hyper_index(self, index: int | None = None,
+                        page_size: int = 10) -> dict:
+        """Retrieves various information for dataset page (deletion resilient)
 
         Args:
             index: current start index of return page
@@ -50,7 +47,26 @@ class Server:
         Returns:
             Dictionary containing relevant page info in key-value pairs
         """
-        assert type(index) is int and type(page_size) is int
-        assert index > 0 and page_size > 0
+        dataset: dict = self.indexed_dataset()
+        dataset_length: int = len(dataset)
 
-        return {}
+        assert type(index) is int and type(page_size) is int
+        assert index >= 0 and index < dataset_length
+        assert page_size > 0
+
+        data: list = []
+        current_index: int = index
+
+        while len(data) < page_size:
+            if current_index in dataset:
+                data.append(dataset[current_index])
+            current_index += 1
+            if current_index > dataset_length:
+                break
+
+        return {
+            "index": index,
+            "data": data,
+            "page_size": page_size,
+            "next_index": current_index
+        }
