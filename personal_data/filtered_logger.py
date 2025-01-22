a8211235fd6d3244aa19d042196856b7e687c076
EliBrank <ezb.business@pm.me>
2025-01-22 04:10:37 +0000

    create function using regex to replace values with specified obfuscation

---
+++ personal_data/filtered_logger.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+"""Creates filter_datum function"""
+
+import re
+
+
+def filter_datum(fields: list[str], redaction: str,
+                 message: str, separator: str) -> str:
+    """Obfuscates log message
+
+    Args:
+        fields: List of all fields (strings) to obfuscate
+        redaction: String used to for obfuscating fields
+        message: String for the log line
+        separator: Character separating all fields in the log line (message)
+
+    Returns:
+        Log message string with obfuscation
+    """
+    filtered_string: str = ""
+    for field in fields:
+        pattern: str = f"{field}=([^{separator}]*)"
+        repl: str = f"{field}={redaction}"
+        filtered_string += re.sub(pattern, repl, message)
+    return filtered_string
