7b6d25de300ebd496eb6a73989762c187a9e2db6
EliBrank <ezb.business@pm.me>
2025-01-23 23:00:03 +0000

    add class to define format method - formats logged message using predefined attributes

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -4,6 +4,7 @@
 
 import re
 from typing import List
+import logging
 
 
 def filter_datum(fields: List[str], redaction: str,
@@ -24,3 +25,21 @@ def filter_datum(fields: List[str], redaction: str,
         repl: str = f"{field}={redaction}"
         message = re.sub(pattern, repl, message)
     return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """Redacting Formatter class"""
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super().__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """Format, obfuscate input record using defined attributes"""
+        message_formatted: str = super().format(record)
+        return filter_datum(self.fields, self.REDACTION,
+                            message_formatted, self.SEPARATOR)
