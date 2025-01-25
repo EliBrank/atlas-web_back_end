cc0016e5e3359d1138ff18b6133a1eca04261ec3
EliBrank <ezb.business@pm.me>
2025-01-25 00:11:32 +0000

    remove type hinting for connections

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -54,10 +54,6 @@ def get_db() -> mysql.connector.connection.MySQLConnection:
     password = os.getenv("PERSONAL_DATA_DB_PASSWORD")
     host_name = os.getenv("PERSONAL_DATA_DB_HOST")
     db_name = os.getenv("PERSONAL_DATA_DB_NAME")
-    # print(f"user: {user}")
-    # print(f"password: {password}")
-    # print(f"host: {host_name}")
-    # print(f"db: {db_name}")
 
     connections: dict = {
         "user": user,
@@ -66,10 +62,9 @@ def get_db() -> mysql.connector.connection.MySQLConnection:
         "database": db_name
     }
 
-    connector_obj: mysql.connector.connection.MySQLConnection = \
-        mysql.connector.connection.MySQLConnection(
-            mysql.connector.connect(**connections)
-        )
+    connector_obj = mysql.connector.connection.MySQLConnection(
+        mysql.connector.connect(**connections)
+    )
 
     return connector_obj
 
