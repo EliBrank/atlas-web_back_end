45af98336792937811601329c185a4722b8212ec
EliBrank <ezb.business@pm.me>
2025-01-26 03:16:40 +0000

    move BasicAuth class to own module

--- Basic_authentication/api/v1/app.py
+++ Basic_authentication/api/v1/app.py
@@ -18,7 +18,7 @@ CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth_type: str = os.getenv("AUTH_TYPE", "auth")
 auth = None
 if auth_type == "basic_auth":
-    from api.v1.auth.auth import BasicAuth
+    from api.v1.auth.basic_auth import BasicAuth
     auth = BasicAuth()
 else:
     from api.v1.auth.auth import Auth
--- Basic_authentication/api/v1/auth/auth.py
+++ Basic_authentication/api/v1/auth/auth.py
@@ -33,10 +33,3 @@ class Auth:
         """Gets user
         """
         return None  # pyright: ignore
-
-
-class BasicAuth(Auth):
-    """
-    BasicAuth authenication implementation
-    """
-    pass
