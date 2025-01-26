a89bea8d8e76db785a4dd55631f4f17057d83200
EliBrank <ezb.business@pm.me>
2025-01-26 20:29:13 +0000

    create method to extract base64 section of auth in header

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -4,10 +4,25 @@ BasicAuth module
 """
 
 from api.v1.auth.auth import Auth
+from typing import Union
 
 
 class BasicAuth(Auth):
     """
     BasicAuth authenication implementation
     """
-    pass
+
+    def extract_base64_authorization_header(
+        self, authorization_header: str
+    ) -> Union[str, None]:
+        """Takes base64 component from auth value of request header
+        """
+        if (
+            not authorization_header
+            or not isinstance(authorization_header, str)
+            or not authorization_header.startswith("Basic ")
+        ):
+            return
+        extracted_base64: str = authorization_header.split("Basic ", 1)[1]
+
+        return extracted_base64
