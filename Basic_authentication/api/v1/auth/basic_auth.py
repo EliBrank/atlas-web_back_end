dc0a7afd950b526e303bd046045e61cf6bff5c68
EliBrank <ezb.business@pm.me>
2025-01-26 22:12:27 +0000

    create decode base64 method for BasicAuth class

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -5,6 +5,9 @@ BasicAuth module
 
 from api.v1.auth.auth import Auth
 from typing import Union
+import base64
+import binascii
+import sys
 
 
 class BasicAuth(Auth):
@@ -26,3 +29,23 @@ class BasicAuth(Auth):
         extracted_base64: str = authorization_header.split("Basic ", 1)[1]
 
         return extracted_base64
+
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str
+    ) -> Union[str, None]:
+        """Decodes extracted base64 string from request header
+        """
+        if (
+            not base64_authorization_header
+            or not isinstance(base64_authorization_header, str)
+        ):
+            return
+
+        decoded_string: Union[str, None] = None
+        try:
+            decoded_bytes = base64.b64decode(base64_authorization_header)
+            decoded_string = decoded_bytes.decode("utf-8")
+        except binascii.Error:
+            return
+
+        return decoded_string
