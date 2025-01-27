dcddb9e98764d5603d571ae0a90b19fe6acde802
EliBrank <ezb.business@pm.me>
2025-01-27 23:31:34 +0000

    add current_user method to BasicAuth, troubleshoot 500 error

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -4,7 +4,7 @@ BasicAuth module
 """
 
 from api.v1.auth.auth import Auth
-from typing import Union
+from typing import Optional, Optional
 import base64
 import binascii
 from api.v1.views.users import User
@@ -17,7 +17,7 @@ class BasicAuth(Auth):
 
     def extract_base64_authorization_header(
         self, authorization_header: str
-    ) -> Union[str, None]:
+    ) -> Optional[str]:
         """Takes base64 component from auth value of request header
         """
         if (
@@ -31,8 +31,8 @@ class BasicAuth(Auth):
         return extracted_base64
 
     def decode_base64_authorization_header(
-            self, base64_authorization_header: str
-    ) -> Union[str, None]:
+            self, base64_authorization_header: Optional[str]
+    ) -> Optional[str]:
         """Decodes extracted base64 string from request header
         """
         if (
@@ -41,18 +41,18 @@ class BasicAuth(Auth):
         ):
             return
 
-        decoded_string: Union[str, None] = None
+        decoded_string: Optional[str] = None
         try:
             decoded_bytes = base64.b64decode(base64_authorization_header)
             decoded_string = decoded_bytes.decode("utf-8")
-        except binascii.Error:
+        except (binascii.Error, UnicodeDecodeError):
             return
 
         return decoded_string
 
     def extract_user_credentials(
-            self, decoded_base64_authorization_header: str
-    ) -> tuple[Union[str, None], Union[str, None]]:
+            self, decoded_base64_authorization_header: Optional[str]
+    ) -> tuple[Optional[str], Optional[str]]:
         """Gets user email, password from request header
         """
         if (
@@ -67,9 +67,9 @@ class BasicAuth(Auth):
         return (email, password)
 
     def user_object_from_credentials(
-            self, user_email: str, user_pwd: str
-    ) -> Union[User, None]:
-        """Gets user object from credentials
+            self, user_email: Optional[str], user_pwd: Optional[str]
+    ) -> Optional[User]:
+        """Gets user instance from credentials
         """
         if (
             not user_email
@@ -79,11 +79,40 @@ class BasicAuth(Auth):
         ):
             return
 
-        users: list[User] = User.search({"email": user_email})
-        if not users:
+        try:
+            users: list[User] = User.search({"email": user_email})
+            for user in users:
+                if user.is_valid_password(user_pwd):
+                    return user
+        except Exception:
             return
-        for user in users:
-            if user.is_valid_password(user_pwd):
-                return user
 
-        return
+    def current_user(self, request=None) -> Optional[User]:
+        """Gets user instance from request object
+        """
+        auth_header: Optional[str] = self.authorization_header(request)
+
+        auth_header_b64: Optional[str] = (
+            auth_header
+            and self.extract_base64_authorization_header(auth_header)
+        )
+
+        auth_header_decoded: Optional[str] = (
+            auth_header_b64 and
+            self.decode_base64_authorization_header(auth_header_b64)
+        )
+
+        credentials: tuple[Optional[str], Optional[str]] = (
+            self.extract_user_credentials(auth_header_decoded)
+            if auth_header_decoded else (None, None)
+        )
+
+        user: Optional[User] = (
+            self.user_object_from_credentials(*credentials)
+            if credentials 
+            and credentials[0] is not None
+            and credentials[1] is not None
+            else None
+        )
+
+        return user
