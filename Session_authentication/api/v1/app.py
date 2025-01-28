4c618a1da36dd9f7a09449f16ac0dfbed48e82a3
EliBrank <ezb.business@pm.me>
2025-01-28 19:45:47 +0000

    edit check for auth header to also check for session cookie

--- Session_authentication/api/v1/app.py
+++ Session_authentication/api/v1/app.py
@@ -29,7 +29,8 @@ else:
 
 EXCLUDED_PATHS: list = ["/api/v1/status/",
                         "/api/v1/unauthorized/",
-                        "/api/v1/forbidden/"]
+                        "/api/v1/forbidden/",
+                        "/api/v1/auth_session/login/"]
 
 
 @app.errorhandler(404)
@@ -59,7 +60,10 @@ def check_request():
     """
     if not auth or not auth.require_auth(request.path, EXCLUDED_PATHS):
         return
-    if not auth.authorization_header(request):
+    if (
+        not auth.authorization_header(request)
+        and not auth.session_cookie(request)
+    ):
         abort(401)
     if not auth.current_user(request):
         abort(403)
