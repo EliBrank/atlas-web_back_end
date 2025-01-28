a152a34840bd0531ea8dba34470b715a38288d4d
EliBrank <ezb.business@pm.me>
2025-01-28 18:23:31 +0000

    create method for initializing session for a user

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -3,6 +3,7 @@
 SessionAuth module
 """
 
+import uuid
 from api.v1.auth.auth import Auth
 from typing import Optional
 
@@ -11,4 +12,15 @@ class SessionAuth(Auth):
     """
     SessionAuth authenication implementation
     """
-    pass
+    user_id_by_session_id: dict = {}
+
+    def create_session(self, user_id: Optional[str] = None) -> Optional[str]:
+        """Creates session ID for given user ID
+        """
+        if not user_id or not isinstance(user_id, str):
+            return
+        session_id: str = str(uuid.uuid4())
+
+        self.user_id_by_session_id.update({session_id: user_id})
+
+        return session_id
