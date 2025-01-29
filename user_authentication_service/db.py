406f9e267efb384933475a795dabfac8f66f1189
EliBrank <ezb.business@pm.me>
2025-01-29 23:12:39 +0000

    Add missing function annotation

--- user_authentication_service/db.py
+++ user_authentication_service/db.py
@@ -48,7 +48,7 @@ class DB:
 
         return user
 
-    def find_user_by(self, **kwargs):
+    def find_user_by(self, **kwargs) -> User:
         """Find first user matching key word input
 
         Args:
