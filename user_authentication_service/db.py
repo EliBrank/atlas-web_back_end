fcf2fc538c0c29b5b90cfc613bd0695d51c72268
EliBrank <ezb.business@pm.me>
2025-01-29 23:11:32 +0000

    correct main files, update DB with find_user_by method

--- user_authentication_service/db.py
+++ user_authentication_service/db.py
@@ -7,6 +7,12 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
+from sqlalchemy.exc import InvalidRequestError
+try:
+    # Older SQLAlchemy
+    from sqlalchemy.orm.exc import NoResultFound  # pyright: ignore
+except ImportError:
+    from sqlalchemy.exc import NoResultFound
 
 from user import Base, User
 
@@ -18,7 +24,7 @@ class DB:
     def __init__(self) -> None:
         """Initialize a new DB instance
         """
-        self._engine = create_engine("sqlite:///a.db", echo=False)
+        self._engine = create_engine("sqlite:///a.db")
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
@@ -33,12 +39,28 @@ class DB:
         return self.__session
 
     def add_user(self, email: str, hashed_password: str) -> User:
-        """ Add new user
+        """Saves user to database
         """
+        user = User(email=email, hashed_password=hashed_password)
 
-        new_user = User(email=email, hashed_password=hashed_password)
-
-        self._session.add(new_user)
+        self._session.add(user)
         self._session.commit()
-        self._session.refresh(new_user)
-        return new_user
+
+        return user
+
+    def find_user_by(self, **kwargs):
+        """Find first user matching key word input
+
+        Args:
+            kwargs: keyword arguments for filtering query
+
+        Returns:
+            first matching User instance
+        """
+        try:
+            user = self._session.query(User).filter_by(**kwargs).first()
+            if not user:
+                raise NoResultFound
+            return user
+        except InvalidRequestError:
+            raise
