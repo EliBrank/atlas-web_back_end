2fc584bb721b5384b0f64c12a888b1cab9237581
EliBrank <ezb.business@pm.me>
2025-01-30 01:46:27 +0000

    move hashing function inside Auth class, use try/except to catch NoResultFound error

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -4,37 +4,47 @@
 Auth module
 """
 import bcrypt
+from db import DB
+from user import Base, User
+try:
+    # Older SQLAlchemy
+    from sqlalchemy.orm.exc import NoResultFound  # pyright: ignore
+except ImportError:
+    from sqlalchemy.exc import NoResultFound
 
 
-def _hash_password(password: str) -> bytes:
-    """Creates hash of input password with bcrypt
 
-    Args:
-        password: String to be hashed, salted
 
-    Return:
-        Hashed bytes object from password
+class Auth:
+    """Auth class to interact with authentication database
     """
-    pw_bytes: bytes = password.encode("utf-8")
-    salt = bcrypt.gensalt()
-    pw_hashed: bytes = bcrypt.hashpw(pw_bytes, salt)
 
-    return pw_hashed
+    def __init__(self):
+        self._db = DB()
 
-# class Auth:
-#     """Auth class
-#     """
+    def _hash_password(self, password: str) -> bytes:
+        """Creates hash of input password with bcrypt
 
-    # def is_valid(hashed_password: bytes, password: str) -> bool:
-    #     """Checks if password matches provided hash
-    #
-    #     Args:
-    #         hashed_password: Hashed string to check password against
-    #         password: Unhashed string to check
-    #
-    #     Return:
-    #         True if password and hash match, else false
-    #     """
-    #     pw_bytes: bytes = password.encode("utf-8")
-    #
-    #     return bcrypt.checkpw(pw_bytes, hashed_password)
+        Args:
+            password: String to be hashed, salted
+
+        Return:
+            Hashed bytes object from password
+        """
+        pw_bytes: bytes = password.encode("utf-8")
+        salt = bcrypt.gensalt()
+        pw_hashed: bytes = bcrypt.hashpw(pw_bytes, salt)
+
+        return pw_hashed
+
+    def register_user(self, email: str, password: str) -> User:
+        """Creates user object from email, password
+        """
+        try:
+            if self._db.find_user_by(email=email):
+                raise ValueError(f"User {email} already exists")
+        except NoResultFound:
+            pass
+
+        hashed_password: str = str(self._hash_password(password))
+        return self._db.add_user(email, hashed_password)
