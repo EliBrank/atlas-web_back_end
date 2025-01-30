39e3f371f2c41821fbe834c263f0ae92262906db
EliBrank <ezb.business@pm.me>
2025-01-30 02:56:06 +0000

    create new endpoint for users, function to register user

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -2,18 +2,40 @@
 """
 Basic Flask app module
 """
+from typing import Optional
 from flask import Flask, jsonify, abort, request
 from flask.typing import ResponseReturnValue
+from auth import Auth
 
+AUTH = Auth()
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
 
 @app.route("/", methods=["GET"])
 def welcome_message() -> ResponseReturnValue:
-    """Greets the user
+    """ GET /
+    Greets the user
     """
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route("/users", methods=["POST"])
+def register_user() -> ResponseReturnValue:
+    """ POST /users
+    Creates user if not already in database
+    """
+    email: str = str(request.form.get("email"))
+    password: str = str(request.form.get("password"))
+
+    try:
+        AUTH.register_user(email, password)
+        return jsonify({
+            "email": "<registered email>",
+            "message": "user created"
+        })
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == "__main__":
