35fc756a138515a67c0119a1781892126e3fc031
EliBrank <ezb.business@pm.me>
2025-01-30 06:50:58 +0000

    create method in Auth to reset password

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -102,9 +102,7 @@ class Auth:
         try:
             self._db.update_user(user_id, session_id=None)
         except ValueError:
-            pass
-
-        return None
+            raise
 
     def get_reset_password_token(self, email: str) -> str:
         """Create reset token (UUID) for user
@@ -115,5 +113,20 @@ class Auth:
             reset_token: str = _generate_uuid()
             self._db.update_user(user_id, reset_token=reset_token)
             return reset_token
+        except NoResultFound:
+            raise ValueError
+
+    def update_password(self, reset_token: str, password: str) -> None:
+        """Gets user from token, updates password
+        """
+        try:
+            user = self._db.find_user_by(reset_token=reset_token)
+            user_id: int = cast(int, user.id)
+            hashed_password = _hash_password(password)
+            self._db.update_user(
+                user_id,
+                reset_token=reset_token,
+                hashed_password=hashed_password
+            )
         except NoResultFound:
             raise ValueError
