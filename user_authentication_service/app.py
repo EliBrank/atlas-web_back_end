a6e742a655c201ba087efe7fa6c09477c7c8ad39
EliBrank <ezb.business@pm.me>
2025-01-30 02:03:01 +0000

    set up basic flask app with greeting endpoint

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -1,28 +1,20 @@
 #!/usr/bin/env python3
 """
-Route module for the API
+Basic Flask app module
 """
-from os import getenv
-from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
-from flask_cors import (CORS, cross_origin)
-import os
-from typing import Union
-
+from flask.typing import ResponseReturnValue
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
-app.register_blueprint(app_views)
-CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
-auth_type: str = os.getenv("AUTH_TYPE", "auth")
-auth = None
-if auth_type == "basic_auth":
-    from api.v1.auth.basic_auth import BasicAuth
-    auth = BasicAuth()
-elif auth_type == "session_auth":
-    from api.v1.auth.session_auth import SessionAuth
-    auth = SessionAuth()
-else:
-    from api.v1.auth.auth import Auth
-    auth = Auth()
+
+@app.route("/", methods=["GET"])
+def welcome_message() -> ResponseReturnValue:
+    """Greets the user
+    """
+    return jsonify({"message": "Bienvenue"})
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
