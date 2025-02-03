0be77e54061930e78057ea45bcc78c695415bfcb
EliBrank <ezb.business@pm.me>
2025-02-03 20:58:09 +0000

    set up mock for requests object, check get_json function

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 
 from typing import Any, Mapping, Sequence
-from utils import access_nested_map
+from utils import access_nested_map, get_json
 import unittest
+from unittest.mock import Mock, patch
 from parameterized import parameterized
 
 
@@ -33,3 +34,23 @@ class TestAccessNestedMap(unittest.TestCase):
         """
         with self.assertRaises(KeyError):
             access_nested_map(nested_map, path)
+
+
+class TestGetJson(unittest.TestCase):
+    """Test suite for get_json
+    """
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False})
+    ])
+    @patch("requests.get")
+    def test_get_json(self, test_url, test_payload, mock_get):
+        """test get_json with mocked requests object
+        """
+        mock_response = Mock()
+        mock_response.json.return_value = test_payload
+        mock_get.return_value = mock_response
+        result = get_json(test_url)
+
+        mock_get.assert_called_once_with(test_url)
+        self.assertEqual(result, test_payload)
