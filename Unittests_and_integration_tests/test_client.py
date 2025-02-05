f32966e5fa52676deba75fc95c6f44e96cf31fc0
EliBrank <ezb.business@pm.me>
2025-02-05 22:00:49 +0000

    finish integration test for public repos method

--- Unittests_and_integration_tests/test_client.py
+++ Unittests_and_integration_tests/test_client.py
@@ -4,10 +4,12 @@
 Tests for functions in client module
 """
 
+from typing import Any, Dict, List
 from client import GithubOrgClient
 import unittest
-from unittest.mock import PropertyMock, patch
-from parameterized import parameterized
+from unittest.mock import Mock, PropertyMock, patch
+from parameterized import parameterized, parameterized_class
+from fixtures import TEST_PAYLOAD
 
 
 class TestGithubOrgClient(unittest.TestCase):
@@ -76,3 +78,52 @@ class TestGithubOrgClient(unittest.TestCase):
         repo_has_license: bool = request.has_license(repo, license)
 
         self.assertEqual(repo_has_license, expected)
+
+
+@parameterized_class(
+    ("org_payload", "repos_payload", "expected_repos", "apache2_repos"),
+    # TEST_PAYLOAD has exactly four elements in the same order as above
+    TEST_PAYLOAD
+)
+class TestIntegrationGithubOrgClient(unittest.TestCase):
+    """Integration test suite for GithubOrgClient
+    """
+    # types defined for parameterized_class attributes for pyright
+    org_payload: Dict[str, str]
+    repos_payload: List[Dict[str, Any]]
+    expected_repos: List[str]
+    apache2_repos: List[str]
+
+    @classmethod
+    def setUpClass(cls):
+        """Sets up class for testing
+        """
+        def get_payload(url):
+            """Gets mock response from url
+            """
+            mock_response = Mock()
+            org_pattern = GithubOrgClient.ORG_URL.format(org="google")
+
+            if url == org_pattern:
+                mock_response.json.return_value = cls.org_payload
+            elif url == cls.org_payload.get("repos_url"):
+                mock_response.json.return_value = cls.repos_payload
+            else:
+                mock_response.json.return_value = {}
+
+            return mock_response
+
+        cls.get_patcher = patch("requests.get", side_effect=get_payload)
+        cls.mock_get = cls.get_patcher.start()
+
+    @classmethod
+    def tearDownClass(cls):
+        """Tears down class after testing
+        """
+        cls.get_patcher.stop()
+
+    def test_public_repos(self):
+        """Tests that public_repos method gets correct data from fixtures
+        """
+        request = GithubOrgClient("google")
+        self.assertEqual(request.public_repos(), self.expected_repos)
