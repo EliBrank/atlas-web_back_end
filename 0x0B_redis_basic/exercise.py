b01a05c725656bda5e37c528fe0b4146b4faba1f
EliBrank <ezb.business@pm.me>
2025-02-08 02:18:18 +0000

    set up Cache class to create redis cache, and store method to set values in cache

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -5,3 +5,20 @@ Main exercise module
 """
 
 import redis
+from uuid import uuid4
+
+
+class Cache():
+    """Simple cache class using redis
+    """
+    def __init__(self) -> None:
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    def store(self, data: str | bytes | int | float) -> str:
+        """Saves input data to database with generated uuid key
+        """
+        generated_key: str = str(uuid4())
+        self._redis.set(generated_key, data)
+
+        return generated_key
