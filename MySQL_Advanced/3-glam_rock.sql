a611808844b74f22395e73c81cc2bfe4984efd9b
EliBrank <ezb.business@pm.me>
2025-02-10 00:01:02 -0600

    hard code 2024 as current year for calculation

--- MySQL_Advanced/3-glam_rock.sql
+++ MySQL_Advanced/3-glam_rock.sql
@@ -1,7 +1,8 @@
 -- Lists Glam Rock bands by longevity
 SELECT
     band_name,
-    (COALESCE(split, YEAR(CURDATE())) - formed) AS lifespan
+    -- (COALESCE(split, YEAR(CURDATE())) - formed) AS lifespan
+    (COALESCE(split, 2024) - formed) AS lifespan
 FROM
     metal_bands
 WHERE
