b70ae914b266bd6db9f18c1437f0abe299cfa970
EliBrank <ezb.business@pm.me>
2025-02-09 23:56:55 -0600

    make sql script to get glam rock bands and order by bands' lifespan

---
+++ MySQL_Advanced/3-glam_rock.sql
@@ -0,0 +1,10 @@
+-- Lists Glam Rock bands by longevity
+SELECT
+    band_name,
+    (COALESCE(split, YEAR(CURDATE())) - formed) AS lifespan
+FROM
+    metal_bands
+WHERE
+    style LIKE '%Glam rock%'
+ORDER BY
+    lifespan DESC;
