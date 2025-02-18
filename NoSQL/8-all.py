e1a80a3aecd02c922003b8ae63407231cd17308b
EliBrank <ezb.business@pm.me>
2025-02-18 16:44:04 -0600

    use short circuit evaluator on list_all return to get empty list if no documents in colleciton

--- NoSQL/8-all.py
+++ NoSQL/8-all.py
@@ -8,4 +8,4 @@ Module for list_all function
 def list_all(mongo_collection):
     """Retrieves all documents in collection
     """
-    return mongo_collection.find()
+    return list(mongo_collection.find()) or []
