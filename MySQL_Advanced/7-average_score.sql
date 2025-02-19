736c3ccf2fe41649f0354a0163fef44d49183def
EliBrank <ezb.business@pm.me>
2025-02-19 16:06:17 -0600

    create procedure script to calc average score for student

---
+++ MySQL_Advanced/7-average_score.sql
@@ -0,0 +1,20 @@
+-- Create stored procedure to find average score for student
+
+DELIMITER $$
+
+CREATE PROCEDURE ComputeAverageScoreForUser(
+    IN p_user_id INT
+)
+BEGIN
+    DECLARE v_average_score DECIMAL;
+
+    SELECT AVG(score) INTO v_average_score
+    FROM corrections
+    WHERE user_id = p_user_id ;
+
+    UPDATE users
+    SET average_score = v_average_score
+    WHERE id = p_user_id;
+END $$
+
+DELIMITER ;
