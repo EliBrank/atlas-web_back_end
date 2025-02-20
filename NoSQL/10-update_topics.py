9e9773fe2f3a5c98885481922e71c25b35d7f5e0
EliBrank <ezb.business@pm.me>
2025-02-19 22:50:01 -0600

    create function to update topics for school in collection

--- NoSQL/10-update_topics.py
+++ NoSQL/10-update_topics.py
@@ -1,1 +1,16 @@
 #!/usr/bin/env python3
+
+"""
+Module for update_topics function
+"""
+
+
+def update_topics(mongo_collection, name, topics):
+    """Changes topics in school document based on name
+    """
+    query_filter = { "name": name }
+    update_operation = { "$set":
+        { "topics": topics }
+    }
+
+    mongo_collection.update_one(query_filter, update_operation)
