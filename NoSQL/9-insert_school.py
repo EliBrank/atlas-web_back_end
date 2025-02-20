6f749f53c5279ecaa452945ddba77b1d5f3f30bb
EliBrank <ezb.business@pm.me>
2025-02-19 21:11:51 -0600

    create function to insert document with school information

--- NoSQL/9-insert_school.py
+++ NoSQL/9-insert_school.py
@@ -1,1 +1,12 @@
 #!/usr/bin/env python3
+
+"""
+Module for insert_school function
+"""
+
+
+def insert_school(mongo_collection, **kwargs):
+    """Inserts school document with info from kwargs
+    """
+    school_document = mongo_collection.insert_one(kwargs)
+    return school_document.inserted_id
