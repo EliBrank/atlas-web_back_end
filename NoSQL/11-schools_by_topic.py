5d79460e1ed2427992c4ff8540f09d3c106e917f
EliBrank <ezb.business@pm.me>
2025-02-19 22:51:08 -0600

    create function which queries for schools based on topic

--- NoSQL/11-schools_by_topic.py
+++ NoSQL/11-schools_by_topic.py
@@ -1,1 +1,11 @@
 #!/usr/bin/env python3
+
+"""
+Module for schools_by_topic function
+"""
+
+
+def schools_by_topic(mongo_collection, topic):
+    """Gets list of schools which include input topic
+    """
+    return list(mongo_collection.find({ "topics": topic })) or []
