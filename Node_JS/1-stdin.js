02d2fd25bfde64157a18a48ae7ede3f747477af6
EliBrank <ezb.business@pm.me>
2025-02-26 14:01:33 -0600

    create program to get name (user input) and print back

---
+++ Node_JS/1-stdin.js
@@ -0,0 +1,16 @@
+process.stdin.setEncoding('utf8');
+
+process.stdout.write('Welcome to Holberton School, what is your name? ');
+
+process.stdin.on('readable', function() {
+    const chunk = process.stdin.read();
+    if (chunk !== null) {
+        const name = chunk.trim();
+        process.stdout.write(`Your name is: ${name}\n`);
+        process.stdin.emit('end');
+    }
+});
+
+process.stdin.on('end', function() {
+    process.stdout.write('This important software is now closing\n');
+});
