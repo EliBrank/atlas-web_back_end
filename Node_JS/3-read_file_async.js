3227a21d380ee7475d27602ac0537fea3684e160
EliBrank <ezb.business@pm.me>
2025-02-26 23:47:31 -0600

    start working on adaptation of countStudents func using async

---
+++ Node_JS/3-read_file_async.js
@@ -0,0 +1,40 @@
+const fs = require('fs').promises;
+const { parse } = require('csv-parse');
+
+async function countStudents(path) {
+  try {
+    const fileContent = await fs.readFile(path, 'utf8');
+
+    const records = parse(fileContent, {
+      columns: true,
+      skip_empty_lines: true,
+    });
+
+    console.log(`Number of students: ${records.length}`);
+    const fields = new Map();
+
+    for (const record of records) {
+      const { field, firstname } = record;
+
+      // check each field to see if already in fields map
+      if (!fields.has(field)) {
+        // if not, add field with empty array to add student names
+        fields.set(field, []);
+      }
+
+      fields.get(field).push(firstname);
+    }
+
+    // Example output for fields:
+    // CS: [Johann, Arielle, Jonathan],
+    // SWE: [Guillaume, Joseph, Paul, Tommy]
+    for (const [field, students] of fields.entries()) {
+      console.log(`Number of students in ${field}: `
+                + `${students.length}. List: ${students.join(', ')}`);
+    }
+  } catch (e) {
+    throw new Error('Cannot load the database');
+  }
+}
+
+module.exports = countStudents;
