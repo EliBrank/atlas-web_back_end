516e2c27bb5752ddca2aa48647062d3aaf983511
EliBrank <ezb.business@pm.me>
2025-03-05 17:02:35 -0600

    fix hset call(s)

--- queuing_system_in_js/4-redis_advanced_op.js
+++ queuing_system_in_js/4-redis_advanced_op.js
@@ -9,16 +9,18 @@ client.on('connect', () => {
   console.log('Redis client connected to the server');
 });
 
-client.hset(
-  'HolbertonSchools',
-  'Portland', 50,
-  'Seattle', 80,
-  'New York', 20,
-  'Bogota', 20,
-  'Cali', 40,
-  'Paris', 2,
-  redis.print
-);
+const citiesWithValues = {
+  'Portland': 50,
+  'Seattle': 80,
+  'New York': 20,
+  'Bogota': 20,
+  'Cali': 40,
+  'Paris': 2
+}
+
+for (const [key, value] of Object.entries(citiesWithValues)) {
+  client.hset('HolbertonSchools', key, value, redis.print);
+}
 
 client.hgetall('HolbertonSchools', (err, reply) => {
   if (err) {
